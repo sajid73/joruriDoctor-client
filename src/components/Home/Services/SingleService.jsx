59c3a2458a8802786608ee57e1a48ddad0a9a193
Kowshik-18 <kowshikc1093@gmail.com>
2022-06-04 23:42:42 +0600

    updated

--- src/components/Home/Services/SingleService.jsx
+++ src/components/Home/Services/SingleService.jsx
@@ -1,14 +1,22 @@
-import { Paper, Typography } from '@mui/material';
-import React from 'react';
+import { Button, Paper, Typography } from "@mui/material";
+import React from "react";
 
-const SingleService = ({ img, title }) => {
-    return (
-        <Paper elevation={3} sx={{ textAlign: 'center', minWidth: '250px', p: 5, m:3 }}>
-            <Typography textAlign={'center'} variant="h5" color="initial">{title}</Typography>
-            <img width={80} src={img} alt={title} />
-            <Typography variant="body1" color="GrayText">Get your appointment</Typography>
-        </Paper>
-    );
+const SingleService = ({ img, title, des }) => {
+  return (
+    <Paper
+      elevation={6}
+      sx={{ textAlign: "center", maxWidth: "300px", p: 5, my: 3 }}
+    >
+      <img width={80} src={img} alt={title} />
+      <Typography variant="h5" component="div" fontWeight={"bold"}>
+        {title}
+      </Typography>
+      <Typography variant="body2" color="black">
+        {des}
+      </Typography>
+      <Button size="small">Learn More</Button>
+    </Paper>
+  );
 };
 
 export default SingleService;
